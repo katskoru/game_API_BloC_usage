8a0886bd3392e28ee3c9d9ef1aee8d9036140848
katskoru <katskoru@gmail.com>
2022-04-25 23:58:40 +0200

    landing page look

--- lib/repo/screens/landing_page.dart
+++ lib/repo/screens/landing_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:glassmorphism/glassmorphism.dart';
+import '../../models/data_model.dart';
 import '../bloc/game_data_bloc.dart';
 
 class LandingPage extends StatelessWidget {
@@ -8,19 +9,116 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<GameDataBloc, GameDataState>(
-      builder: (context, state) {
-        if (state is GameDataInitialState) {
-          context.read<GameDataBloc>().add(LoadGameDataEvent());
-          return const CircularProgressIndicator();
-        } else if (state is LoadingState) {
-          return const CircularProgressIndicator();
-        } else if (state is LoadedState) {
-          return Text("Data is loaded");
-        } else if (state is GameDataErrorState) {
-          return Text("Something went wrong");
-        }
-        return const Text("Error");
+    return Scaffold(
+      body: BlocBuilder<GameDataBloc, GameDataState>(
+        builder: (context, state) {
+          // context.read<GameDataBloc>().add(GameDataLoading());
+          if (state is GameDataInitialState) {
+            context.read<GameDataBloc>().add(LoadGameDataEvent());
+            return const CircularProgressIndicator();
+          } else if (state is LoadingState) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is LoadedState) {
+            return buildGameModel(state.apiResult);
+          } else if (state is GameDataErrorState) {
+            return const Center(
+              child: Text("Uh oh! 😭 Something went wrong!"),
+            );
+          }
+          return const Text("Error");
+        },
+      ),
+    );
+  }
+
+  Widget buildGameModel(List<DataModel> apiResult) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: apiResult.length,
+      itemBuilder: (BuildContext context, int index) {
+        final DataModel dataModel = apiResult[index];
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 8),
+          child: InkWell(
+            onTap: () {},
+            child: SizedBox(
+              width: double.infinity,
+              child: Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image(
+                      height: 230,
+                      width: MediaQuery.of(context).size.width / 1.05,
+                      fit: BoxFit.cover,
+                      image: NetworkImage(dataModel.image),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    child: GlassmorphicContainer(
+                      width: MediaQuery.of(context).size.width / 1.05,
+                      height: 120,
+                      border: 0,
+                      borderRadius: 0,
+                      blur: 20,
+                      alignment: Alignment.bottomCenter,
+                      linearGradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          const Color(0xFFffffff).withOpacity(0.1),
+                          const Color(0xFFFFFFFF).withOpacity(0.05),
+                        ],
+                        stops: const [
+                          0.1,
+                          1,
+                        ],
+                      ),
+                      borderGradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          const Color(0xFFffffff).withOpacity(0.5),
+                          const Color((0xFFFFFFFF)).withOpacity(0.5),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: double.infinity,
+                              child: Text(
+                                dataModel.title,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 4,
+                            ),
+                            Text(
+                              "Platforms: " + dataModel.platforms,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
       },
     );
   }
